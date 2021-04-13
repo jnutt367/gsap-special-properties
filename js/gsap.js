6c3fb99e74a3a66bb0fb1024bcb5f732c6321a95
Jason Nutt <jnutt367@gmail.com>
2021-04-13 10:31:52 -0400

    commit freds race

--- js/gsap.js
+++ js/gsap.js
@@ -1,8 +1,15 @@
+gsap.to("h1", { duration: 2, color: "white", textAlign: "center" });
+
 //add code to experiment with stagger object
 gsap.to("#freds img", {
-    y: -250,
+    y: "-700",
+    scale: 1.5,
+    duration: 1,
+    ease: "none",
     stagger: {
-        amount: 1,
-        from: "end"
+        amount: 2,
+        from: "edges",
+        ease: "power2.inOut",
+
     }
 });
